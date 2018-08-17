02e0e1c7ce1db0dd94b8e238667ed62444cb0edc
arpitasahool <arpitasahoo945@gmail.com>
2018-08-17 14:41:42 +0530

    the bug is resolved

---
+++ assignment2.py
@@ -0,0 +1,31 @@
+#1. Print Something
+
+print("hello")
+
+#2. Concatinate Strings
+
+print("hello" + " arpita")
+
+#3. Print 3 User Defined Variables
+
+a=input("value1 ")
+b=input("value2 ")
+c=input("value3 ")
+print(a,b,c) 
+
+#4. Print Let's Get Started
+
+print("Let's Get Started")
+
+#5. Print the Given Values Using Placeholders
+
+s="Acadview" 
+course="Python" 
+fees=5000
+print("%s %s %s" %(s,course ,fees))
+
+#6. Find the Area of Circle With User Defined Radius
+
+r=input("enter radius of circle")
+a=3.14*r*r
+print(a)
